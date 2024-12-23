d631c433086984357d1ff9d4f399f847891d9ae7
hazem <hazemnasser3050@gmail.com>
2024-12-23 23:21:00 +0200

    test

---
+++ Company/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Company.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
